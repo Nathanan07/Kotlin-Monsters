6499041874bc3805ca0fe8249c892df1b75e5463
Songcharoenkul <nsongcharoenkul@gmail.com>
2026-09-25 09:06:38 +0200

    Déclaration de la fonction changeCouleur()

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun changeCouleur(message: String, couleur:String=""): String {
+    val reset = "\u001B[0m"
+    val codeCouleur = when (couleur.lowercase()) {
+        "rouge" -> "\u001B[31m"
+        "vert" -> "\u001B[32m"
+        "jaune" -> "\u001B[33m"
+        "bleu" -> "\u001B[34m"
+        "magenta" -> "\u001B[35m"
+        "cyan" -> "\u001B[36m"
+        "blanc" -> "\u001B[37m"
+        else -> "" // pas de couleur si non reconnu
+    }
+    return "$codeCouleur$message$reset"
+}
+
+
+fun main () {
+
+}
